baef74dcd3e818656b0789ed9dfbe3942d1a403f
Thomas Mailund <tm@kvantify.dk>
2024-07-02 19:12:32 +0200

    Fixed bits algorithms

--- stralg/searching/bits.py
+++ stralg/searching/bits.py
@@ -1,10 +1,9 @@
 from __future__ import annotations
 
-from collections import defaultdict
 from typing import Any, Iterator
 
 
-class IntBitVector:
+class BitVector:
     size: int
     bits: int
     mask: int
@@ -22,54 +21,57 @@ class IntBitVector:
     def __int__(self) -> int:
         return self.bits
 
-    def __lshift__(self, other: int) -> IntBitVector:
+    def __lshift__(self, other: int) -> BitVector:
         assert other >= 0, "Negative shift count"
-        return IntBitVector(self.size, self.bits << other)
+        return BitVector(self.size, self.bits << other)
 
-    def __add__(self, other: Any) -> IntBitVector:
-        return IntBitVector(self.size, self.bits + int(other))
+    def __add__(self, other: Any) -> BitVector:
+        return BitVector(self.size, self.bits + int(other))
 
-    def __or__(self, other: Any) -> IntBitVector:
-        return IntBitVector(self.size, self.bits | int(other))
+    def __or__(self, other: Any) -> BitVector:
+        return BitVector(self.size, self.bits | int(other))
 
-    def __and__(self, other: Any) -> IntBitVector:
-        return IntBitVector(self.size, self.bits & int(other))
+    def __and__(self, other: Any) -> BitVector:
+        return BitVector(self.size, self.bits & int(other))
 
-    def __invert__(self) -> IntBitVector:
-        return IntBitVector(self.size, ~self.bits & self.mask)
+    def __invert__(self) -> BitVector:
+        return BitVector(self.size, ~self.bits & self.mask)
 
     def __getitem__(self, index: int) -> bool:
         return (self.bits & (1 << index)) != 0
 
 
-def match_bv(p: str, a: str) -> IntBitVector:
-    """Return a bitvector of locaations where p[j] == a."""
+def match_bv(p: str, a: str) -> BitVector:
+    """Return a bitvector of locations where p[j] == a."""
     bits = 0
-    for b in p:
+    for b in reversed(p):
         bits = (bits << 1) + int(a == b)
-    return IntBitVector(len(p), bits)
+    return BitVector(len(p), bits)
 
 
 def shift_and(x: str, p: str) -> Iterator[int]:
-    t: dict[str, IntBitVector] = defaultdict(lambda: IntBitVector(len(p), 0))
-    t.update({a: match_bv(p, a) for a in set(p)})
-    print(t)
+    assert len(p) > 0, "Pattern cannot be empty"
 
-    bv = IntBitVector(len(p), 0)
+    missing = BitVector(len(p), 0)
+    t = {a: match_bv(p, a) for a in set(p)}
+
+    status = BitVector(len(p), 0)
     for i, a in enumerate(x):
-        bv = ((bv << 1) + 1) & t[a]
-        if bv[len(p) - 1]:
+        match = t[a] if a in t else missing
+        status = ((status << 1) + 1) & match
+        if status[len(p) - 1]:
             yield i - len(p) + 1
 
 
 def shift_or(x: str, p: str) -> Iterator[int]:
-    t: dict[str, IntBitVector] = defaultdict(lambda: IntBitVector(len(p), ~0))
-    t.update({a: ~match_bv(p, a) for a in set(p)})
-    print(t)
+    assert len(p) > 0, "Pattern cannot be empty"
 
-    bv = IntBitVector(len(p), ~0)
+    missing = BitVector(len(p), ~0)
+    t = {a: ~match_bv(p, a) for a in set(p)}
+
+    bv = BitVector(len(p), ~0)
     for i, a in enumerate(x):
-        print(a, t[a])
-        bv = (bv << 1) | t[a]
+        match = t[a] if a in t else missing
+        bv = (bv << 1) | match
         if not bv[len(p) - 1]:
             yield i - len(p) + 1
