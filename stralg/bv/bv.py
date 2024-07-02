07b4657129626007c912b5879dacb0b814e4a0b5
Thomas Mailund <tm@kvantify.dk>
2024-07-02 11:09:31 +0200

    Int bitvector and SHIFT-AND & SHIFT-OR

--- stralg/bv/bv.py
+++ stralg/bv/bv.py
@@ -3,6 +3,7 @@
 from __future__ import annotations
 
 from array import array
+from typing import Any
 
 TYPECODE = "B"  # Q
 BYTE_BITS = 8
@@ -58,15 +59,61 @@ class BitVector:
         return result
 
 
-bv = BitVector(12)
-bv[1] = True
-bv[7] = True
-bv[8] = True
-bv[11] = True
-# bv[63] = True
-# bv[64] = True
-print(bv)
-print(bv << 0)
-print(bv << 1)
-print(bv << 8)
-print(bv << 9)
+class IntBitVector:
+    size: int
+    bits: int
+    mask: int
+
+    def __init__(self, size: int, bits: int | str = 0) -> None:
+        self.size = size
+        self.mask = (1 << size) - 1
+        match bits:
+            case int():
+                self.bits = bits & self.mask
+            case str():
+                self.bits = int(bits, 2) & self.mask
+
+    def __str__(self) -> str:
+        return format(self.bits, f"0{self.size}b")
+
+    def __lshift__(self, other: int) -> IntBitVector:
+        assert other >= 0, "Negative shift count"
+        return IntBitVector(self.size, self.bits << other)
+
+    def __add__(self, other: Any) -> IntBitVector:
+        return IntBitVector(self.size, self.bits + int(other))
+
+    def __int__(self) -> int:
+        return self.bits
+
+    def __or__(self, other: Any) -> IntBitVector:
+        return IntBitVector(self.size, self.bits | int(other))
+
+    def __and__(self, other: Any) -> IntBitVector:
+        return IntBitVector(self.size, self.bits & int(other))
+
+    def __getitem__(self, index: int) -> bool:
+        return (self.bits & (1 << index)) != 0
+
+
+p = "aba"
+x = "ababa"
+t = {
+    "a": IntBitVector(3, "101"),
+    "b": IntBitVector(3, "010"),
+}
+bv = IntBitVector(3, 1) & t["a"]
+for i, a in enumerate(x[1:]):
+    bv = ((bv << 1) + 1) & t[a]
+    print(f"bv{i} = {bv}", "*" if bv[2] else "")
+
+print()
+
+t = {
+    "a": IntBitVector(3, "010"),
+    "b": IntBitVector(3, "101"),
+}
+bv = IntBitVector(3, ~1) | t["a"]
+for i, a in enumerate(x[1:]):
+    bv = (bv << 1) | t[a]
+    print(f"bv{i} = {bv}", "*" if not bv[2] else "")
