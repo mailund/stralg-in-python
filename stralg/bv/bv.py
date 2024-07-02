d8365c6b5b8d735e1653743dcd3e1677d45134ad
Thomas Mailund <tm@kvantify.dk>
2024-07-02 08:25:24 +0200

    Left-shift

--- stralg/bv/bv.py
+++ stralg/bv/bv.py
@@ -4,8 +4,9 @@ from __future__ import annotations
 
 from array import array
 
+TYPECODE = "B"  # Q
 BYTE_BITS = 8
-WS = array("Q").itemsize * BYTE_BITS
+WS = array(TYPECODE).itemsize * BYTE_BITS
 
 
 def buckets_needed(items: int, bucket_size: int) -> int:
@@ -19,7 +20,16 @@ class BitVector:
 
     def __init__(self, size: int) -> None:
         self.size = size
-        self.words = array("Q", [0] * buckets_needed(size, WS))
+        self.words = array(TYPECODE, [0] * buckets_needed(size, WS))
+
+    def __len__(self) -> int:
+        return self.size
+
+    def __str__(self) -> str:
+        words = reversed([format(word, f"0{WS}b") for word in self.words])
+        return " ".join(words)
+
+    __repr__ = __str__
 
     def __getitem__(self, index: int) -> bool:
         bucket = index // WS
@@ -39,9 +49,24 @@ class BitVector:
         result = BitVector(self.size)
         word_offset = other // WS
         word_shift = other % WS
+        mask = (1 << WS) - 1
         carry = 0
-        for i in range(self.size - word_offset):
-            left = self.words[i] >> (WS - word_shift)
-            result.words[i + word_offset] = left | carry
-            carry = self.words[i + word_offset] << word_shift
+        for i in range(len(self.words) - word_offset):
+            w = (self.words[i] << word_shift) & mask
+            result.words[i + word_offset] = w | carry
+            carry = self.words[i] >> (WS - word_shift)
         return result
+
+
+bv = BitVector(12)
+bv[1] = True
+bv[7] = True
+bv[8] = True
+bv[11] = True
+# bv[63] = True
+# bv[64] = True
+print(bv)
+print(bv << 0)
+print(bv << 1)
+print(bv << 8)
+print(bv << 9)
